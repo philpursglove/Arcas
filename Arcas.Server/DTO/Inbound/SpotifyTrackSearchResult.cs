1b7fb9ccbd5e5704fbb11787904056c5e8fb9d93
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-07-28 18:46:05 +0100

    Spotify DTOs

---
+++ Arcas.Server/DTO/Inbound/SpotifyTrackSearchResult.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Arcas.Server.DTO.Inbound;
+
+public class SpotifyTrackSearchResult
+{
+    [JsonPropertyName("tracks")]
+    public SpotifyTrackSearchTracks Tracks { get; set; }
+}
+
+public class SpotifyTrackSearchTracks
+{
+    [JsonPropertyName("items")]
+    public SpotifyTrack[] Items { get; set; }
+}
+
+public class SpotifyTrack
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("artists")]
+    public SpotifyArtist[] Artists { get; set; }
+}
+
+public class SpotifyArtist
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
---
+++ Arcas.Server/DTO/Outbound/SpotifyTrack.cs
@@ -0,0 +1,7 @@
+﻿namespace Arcas.Server.DTO.Outbound;
+
+public class SpotifyTrack
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
