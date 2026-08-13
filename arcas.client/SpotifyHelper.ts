d7baf79e03271ffad4993e0a5c591a5190b11b7d
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-08-13 16:13:59 +0100

    Type the functions

--- arcas.client/SpotifyHelper.ts
+++ arcas.client/SpotifyHelper.ts
@@ -1,23 +1,23 @@
-export function generateRandomString(length: number = 64) {
+export function generateRandomString(length: number = 64): string {
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const values = crypto.getRandomValues(new Uint8Array(length));
     return values.reduce((acc, x) => acc + possible[x % possible.length], "");
 }
 
-export async function sha256(plainText: string) {
+export async function sha256(plainText: string): Promise<ArrayBuffer> {
     const encoder = new TextEncoder()
     const data = encoder.encode(plainText)
     return window.crypto.subtle.digest('SHA-256', data)
 }
 
-export function base64encode(input: ArrayBuffer) {
+export function base64encode(input: ArrayBuffer): string {
     return btoa(String.fromCharCode(...new Uint8Array(input)))
         .replace(/=/g, '')
         .replace(/\+/g, '-')
         .replace(/\//g, '_');
 }
 
-export async function getSpotifyClientId() {
+export async function getSpotifyClientId(): Promise<string>      {
     const response = await fetch('Spotify/getclientid');
     if (!response.ok) 
     {
