74b5f1b64ced82dbd418049ed7a8b0fdeb5384ed
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-09-03 20:55:06 +0100

    Flesh out Recents

--- Arcas.Server/RecentPlaylist.cs
+++ Arcas.Server/RecentPlaylist.cs
@@ -1,6 +1,17 @@
-﻿namespace Arcas.Server
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace Arcas.Server
 {
-    public class RecentPlaylist
+    public class RecentPlaylist : ITableEntity
     {
+        public string PartitionKey { get; set; } = "RecentPlaylist";
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
--- Arcas.Server/RecentSetlist.cs
+++ Arcas.Server/RecentSetlist.cs
@@ -1,6 +1,18 @@
-﻿namespace Arcas.Server
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace Arcas.Server
 {
-    public class RecentSetlist
+    public class RecentSetlist : ITableEntity
     {
+        public string PartitionKey { get; set; } = "RecentSetlist";
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+        public string Id { get; set; }
+        public string Artist { get; set; }
+        public string Name { get; set; }
+        public string SetlistId { get; set; }
+        public DateTime EventDate { get; set; }
     }
 }
