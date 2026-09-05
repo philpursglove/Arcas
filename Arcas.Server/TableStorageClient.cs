e12136b136b9996e8c52bc3f76c7107456346027
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-09-05 10:58:55 +0100

    Fill in some TableServiceCLient

--- Arcas.Server/TableStorageClient.cs
+++ Arcas.Server/TableStorageClient.cs
@@ -1,10 +1,25 @@
-﻿namespace Arcas.Server
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace Arcas.Server
 {
-    public class TableStorageClient<T>
+    public class TableStorageClient<T> where T : ITableEntity
     {
+        private readonly TableClient tableClient;
+        public TableStorageClient(string connectionString)
+        {
+            var serviceClient = new TableServiceClient(connectionString);
+            tableClient = serviceClient.GetTableClient(typeof(T).Name);
+        }
+
         public async Task Save(T entity)
         {
+            await tableClient.AddEntityAsync(entity);
+        }
 
+        public async Task Delete(ETag etag)
+        {
+            await tableClient.DeleteEntityAsync(etag);
         }
     }
 }
